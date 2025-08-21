fb92c0fd37f0f5c01e5552c2038d9a59be87c1d4
argyris3 <bigol3@hotmail.com>
2025-08-21 11:02:34 +0300

    fourth  commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 const local = 'http://localhost:8001'
-const production = 'https://vinyl-store-backend-hfjx.onrender.com'
-let api_url = ''
-let mode = "pro "
-if (mode === "pro") {
-    api_url = production
+const production = 'https://vinyl-store-backend-k4g7.onrender.com';
+let api_url = '';
+let mode = 'pro ';
+if (mode === 'pro') {
+  api_url = production;
 } else {
-    api_url = local
+  api_url = local;
 }
 
-
-
 const api = axios.create({
-    baseURL: `${api_url}/api`
-})
+  baseURL: `${api_url}/api`,
+});
 
-export default api
+export default api;
